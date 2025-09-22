f5d782eda4dae83dd8f5a4d1355c2d63a3c50368
Nico Tsiridis <nct23gm@gmail.com>
2025-09-21 20:34:43 -0600

    forward prop start

--- multi_layer_network.c
+++ multi_layer_network.c
@@ -1,25 +1,44 @@
 #include <stdio.h>
 #include <cmatrix.h>
+#include <math.h>
+
+float hidden_function(float x1, float x2) {
+	return pow(x1, 2) + pow(x2, 4) + 7;
+}
+
+Matrix** init_weights(
+	size_t input_size, 
+	size_t output_size, 
+	size_t hidden_layer_nodes, 
+	size_t num_transforms
+) {
+	Matrix **weights = malloc(num_transforms * sizeof(Matrix*));
+	weights[0] = mat_rand(input_size, hidden_layer_nodes);
+	for (int i = 1; i < num_transforms - 1; i++) {
+		weights[i] = mat_rand(hidden_layer_nodes, hidden_layer_nodes);
+	}
+	weights[num_transforms - 1] = mat_rand(hidden_layer_nodes, output_size);
+	return weights;
+}
+
+void forward(Matrix* m, Matrix** weights, int num_transforms) {
+	for (int i = 0; i < num_transforms; i++) {
+		// TODO
+	}
+}
 
 int main() {
 	size_t hidden_layers = 2;
 	size_t hidden_layer_nodes = 2;
-	size_t total_layers = hidden_layers + 1;
+	size_t num_transforms = hidden_layers + 1;
 	size_t input_size = 2;
 	size_t output_size = 1;
 
-	Matrix **layers = malloc(total_layers * sizeof(Matrix*));
-
-	layers[0] = mat_rand(input_size, hidden_layer_nodes);
-	for (int i = 1; i < total_layers - 1; i++) {
-		layers[i] = mat_rand(hidden_layer_nodes, hidden_layer_nodes);
-	}
-	layers[total_layers - 1] = mat_rand(hidden_layer_nodes, output_size);
-
-	for (int i = 0; i < total_layers; i++) {
+	Matrix **weights = init_weights(input_size, output_size, hidden_layer_nodes, num_transforms);
+	for (int i = 0; i < num_transforms; i++) {
 		char label[32];
 		snprintf(label, sizeof(label), "Transform %d -> %d", i, i + 1);
-		mat_print(layers[i], label);
+		mat_print(weights[i], label);
 	}
 
 	//Matrix *m = mat_rand(5, 5);
