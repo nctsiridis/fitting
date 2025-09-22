3d03127e800d214fee775fc1e3de8f52fe0e1dbc
Nico Tsiridis <nct23gm@gmail.com>
2025-09-22 11:06:57 -0600

    forward prop done

--- multi_layer_network.c
+++ multi_layer_network.c
@@ -13,18 +13,22 @@ Matrix** init_weights(
 	size_t num_transforms
 ) {
 	Matrix **weights = malloc(num_transforms * sizeof(Matrix*));
-	weights[0] = mat_rand(input_size, hidden_layer_nodes);
+	weights[0] = mat_rand(hidden_layer_nodes, input_size);
 	for (int i = 1; i < num_transforms - 1; i++) {
 		weights[i] = mat_rand(hidden_layer_nodes, hidden_layer_nodes);
 	}
-	weights[num_transforms - 1] = mat_rand(hidden_layer_nodes, output_size);
+	weights[num_transforms - 1] = mat_rand(output_size, hidden_layer_nodes);
 	return weights;
 }
 
 void forward(Matrix* m, Matrix** weights, int num_transforms) {
 	for (int i = 0; i < num_transforms; i++) {
-		// TODO
+		mat_dot(weights[i], m);
 	}
+}
+
+void backward() {
+	// TODO implement back prop
 }
 
 int main() {
@@ -35,13 +39,12 @@ int main() {
 	size_t output_size = 1;
 
 	Matrix **weights = init_weights(input_size, output_size, hidden_layer_nodes, num_transforms);
-	for (int i = 0; i < num_transforms; i++) {
-		char label[32];
-		snprintf(label, sizeof(label), "Transform %d -> %d", i, i + 1);
-		mat_print(weights[i], label);
-	}
 
-	//Matrix *m = mat_rand(5, 5);
-	//mat_print(m, "my matrix");
+	Matrix *m = mat_rand(2, 1);
+
+	mat_print(m, "before");
+	forward(m, weights, num_transforms);
+	mat_print(m, "after");
+
 	return 0;
 }
