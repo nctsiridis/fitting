94795c27e062be681e1a7c6c3f62f940adcdc704
Nico Tsiridis <nct23gm@gmail.com>
2025-09-29 08:49:25 -0600

    not working

--- multi_layer_network.c
+++ multi_layer_network.c
@@ -2,27 +2,59 @@
 #include <cmatrix.h>
 #include <math.h>
 
+struct {
+	Matrix **weights;
+	Matrix **biases;
+	Matrix **activations;
+	size_t input_size, output_size;
+	size_t num_transforms;
+} Network;
+
 float hidden_function(float x1, float x2) {
 	return pow(x1, 2) + pow(x2, 4) + 7;
 }
 
-Matrix** init_weights(
+Network init_network(
 	size_t input_size, 
 	size_t output_size, 
 	size_t hidden_layer_nodes, 
 	size_t num_transforms
 ) {
+	Network network = malloc(sizeof(Network));
+	
 	Matrix **weights = malloc(num_transforms * sizeof(Matrix*));
 	weights[0] = mat_rand(hidden_layer_nodes, input_size);
 	for (int i = 1; i < num_transforms - 1; i++) {
 		weights[i] = mat_rand(hidden_layer_nodes, hidden_layer_nodes);
 	}
 	weights[num_transforms - 1] = mat_rand(output_size, hidden_layer_nodes);
-	return weights;
+
+	Matrix **biases = malloc(num_transforms * sizeof(Matrix*));
+	for (int i = 0; i < num_transforms - 1; i++) {
+		biases[i] = mat_rand(hidden_layer_nodes, 1);
+	}
+	biases[num_transforms - 1] = mat_rand(output_size, 1);
+
+	Matrix **activations = malloc((num_transforms + 1) * sizeof(Matrix*));
+	activations[0] = mat_rand(input_size, 1);
+	for (int i = 1; i < num_transforms; i++) {
+		activations[i] = mat_rand(hidden_layer_nodes, 1);
+	}
+	activations[num_transforms] = mat_rand(output_size, 1);
+
+	network.weights = weights;
+	network.biases = biases;
+	network.activations = activations;
+	network.input_size = input_size;
+	network.output_size = output_size;
+	network.num_transforms = num_transforms;
+
+	return network;
 }
 
-void forward(Matrix* m, Matrix** weights, int num_transforms) {
-	for (int i = 0; i < num_transforms; i++) {
+void forward(Network *network) {
+	Matrix* m = network->activations[0];
+	for (int i = 0; i < network->num_transforms; i++) {
 		mat_dot(weights[i], m);
 	}
 }
@@ -38,13 +70,9 @@ int main() {
 	size_t input_size = 2;
 	size_t output_size = 1;
 
-	Matrix **weights = init_weights(input_size, output_size, hidden_layer_nodes, num_transforms);
+	Network network = init_network(input_size, output_size, hidden_layer_nodes, num_transforms);
 
-	Matrix *m = mat_rand(2, 1);
-
-	mat_print(m, "before");
-	forward(m, weights, num_transforms);
-	mat_print(m, "after");
+	forward(&network);
 
 	return 0;
 }
